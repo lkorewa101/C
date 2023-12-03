75125927f4d03b64a77ea24b21d5f891348fba4d
KiHyunKo <lkorewa101@gmail.com>
2023-12-03 18:55:43 +0900

    notepad

--- notepad.c
+++ notepad.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    int data = 1;
-    #ifdef DEBUG {
-        data++;
-    }
-    #endif {
-        printf("data = %d", data);
-    }
+// 함수 원형 선언
+int getsum(int n);
+
+int main(void) {
+    int max = 0;
+
+    // 양의 정수 입력 받기
+    printf("1에서 n까지의 합을 구할 n을 입력하시오. >>");
+    scanf("%d", &max);
+
+    // getsum 함수 호출하여 합 구하고 출력
+    printf("1에서 %d까지의 합: %d\n", max, getsum(max));
+
     return 0;
+}
+
+// getsum 함수 정의
+int getsum(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 }
